20e34abf28979fa899f5ca81e613e5f09308c04e
WilliamMbakop <william.mbakop@oclock.school>
2023-08-25 19:38:24 +0200

    route to list one cocktail and template done + umpdate template list all cocktails

--- src/Controller/Admin/CocktailController.php
+++ src/Controller/Admin/CocktailController.php
@@ -2,10 +2,12 @@
 
 namespace App\Controller\Admin;
 
+use App\Entity\Cocktail;
 use App\Repository\CocktailRepository;
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use PhpParser\Node\Stmt\Foreach_;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 
 class CocktailController extends AbstractController
@@ -19,5 +21,17 @@ class CocktailController extends AbstractController
         return $this->render('cocktail/list.html.twig', [
             'cocktails' => $cocktailRepository->findAll(),
         ]);
+    }
+
+    /**
+     * Display one cocktail 
+     * @Route("/admin/cocktails/{id}", name="app_cocktail_show")
+     */
+    public function show(CocktailRepository $cocktailRepository, Cocktail $cocktail): Response
+    {
+        $cocktail = $cocktailRepository->find($cocktail);
+        
+        return $this->render(
+        'cocktail/show.html.twig', ['cocktail' => $cocktail]);
     }
 }
