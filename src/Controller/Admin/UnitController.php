73f1f6e2da9e39f0386abfcc0fb2a0d63218afe6
Celestin-J <celestin.joiris@oclock.school>
2023-08-30 13:58:36 +0200

    crud for unit add

---
+++ src/Controller/Admin/UnitController.php
@@ -0,0 +1,77 @@
+<?php
+
+namespace App\Controller\Admin;
+
+use App\Entity\Unit;
+use App\Form\UnitType;
+use App\Repository\UnitRepository;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class UnitController extends AbstractController
+{
+    /**
+     * @Route("/admin/unites", name="app_unit_index", methods={"GET"})
+     */
+    public function index(UnitRepository $unitRepository): Response
+    {
+        return $this->render('unit/index.html.twig', [
+            'units' => $unitRepository->findAll(),
+        ]);
+    }
+
+    /**
+     * @Route("/admin/unite/ajouter", name="app_unit_new", methods={"GET", "POST"})
+     */
+    public function new(Request $request, UnitRepository $unitRepository): Response
+    {
+        $unit = new Unit();
+        $form = $this->createForm(UnitType::class, $unit);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $unitRepository->add($unit, true);
+
+            return $this->redirectToRoute('app_unit_index', [], Response::HTTP_SEE_OTHER);
+        }
+
+        return $this->renderForm('unit/new.html.twig', [
+            'unit' => $unit,
+            'form' => $form,
+        ]);
+    }
+
+    /**
+     * @Route("/admin/unite/{id}/edit", name="app_unit_edit", methods={"GET", "POST"})
+     */
+    public function edit(Request $request, Unit $unit, UnitRepository $unitRepository): Response
+    {
+        $form = $this->createForm(UnitType::class, $unit);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $unitRepository->add($unit, true);
+
+            return $this->redirectToRoute('app_unit_index', [], Response::HTTP_SEE_OTHER);
+        }
+
+        return $this->renderForm('unit/edit.html.twig', [
+            'unit' => $unit,
+            'form' => $form,
+        ]);
+    }
+
+    /**
+     * @Route("/admin/unite/{id}", name="app_unit_delete", methods={"POST"})
+     */
+    public function delete(Request $request, Unit $unit, UnitRepository $unitRepository): Response
+    {
+        if ($this->isCsrfTokenValid('delete'.$unit->getId(), $request->request->get('_token'))) {
+            $unitRepository->remove($unit, true);
+        }
+
+        return $this->redirectToRoute('app_unit_index', [], Response::HTTP_SEE_OTHER);
+    }
+}
--- src/Entity/Unit.php
+++ src/Entity/Unit.php
@@ -2,11 +2,13 @@
 
 namespace App\Entity;
 
+
 use App\Repository\UnitRepository;
 use Doctrine\Common\Collections\ArrayCollection;
 use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
 use Symfony\Component\Serializer\Annotation\Groups;
+use Symfony\Component\Validator\Constraints as Assert;
 
 
 /**
@@ -25,6 +27,8 @@ class Unit
     /**
      * @ORM\Column(type="string", length=255)
      * @Groups({"cocktailsAllInfo", "units", "propositionsData"})
+     * @Assert\NotBlank
+     * @Assert\Length(max=50)
      */
     private $name;
 
