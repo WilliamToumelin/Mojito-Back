af9c9aca159c27534ba376e7542ab41aa80bfbe2
WilliamMbakop <william.mbakop@oclock.school>
2023-09-02 11:00:55 +0200

    list and delete endpoint comment done

---
+++ src/Controller/Admin/CommentController.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Controller\Admin;
+
+use App\Entity\Comment;
+use App\Form\CommentType;
+use App\Repository\CommentRepository;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class CommentController extends AbstractController
+{
+    /**
+     * @Route("/admin/comments", name="app_comment_index", methods={"GET"})
+     */
+    public function index(CommentRepository $commentRepository): Response
+    {   
+
+        return $this->render('comment/index.html.twig', [
+            'comments' => $commentRepository->findAll(),
+        ]);
+    }
+
+
+    /**
+     * @Route("/admin/comment/{id}/supprimer", name="app_comment_delete", methods={"POST"})
+     */
+    public function delete(Request $request, Comment $comment, CommentRepository $commentRepository): Response
+    {
+        if ($this->isCsrfTokenValid('delete'.$comment->getId(), $request->request->get('_token'))) {
+            $commentRepository->remove($comment, true);
+        }
+
+        return $this->redirectToRoute('app_comment_index', [], Response::HTTP_SEE_OTHER);
+    }
+}
