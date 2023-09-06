8ee5d7b7ac583df5efabffedf6e6f4f7c9fe1643
WilliamMbakop <william.mbakop@oclock.school>
2023-09-06 12:51:18 +0200

    add route to create an admin member

--- src/Controller/Admin/UserController.php
+++ src/Controller/Admin/UserController.php
@@ -16,10 +16,33 @@ class UserController extends AbstractController
      * @Route("/admin/users", name="app_user_index", methods={"GET"})
      */
     public function index(UserRepository $userRepository): Response
-    {   
+    {
 
         return $this->render('user/index.html.twig', [
             'users' => $userRepository->findAll(),
+        ]);
+    }
+
+
+    /**
+     * @Route("/admin/membre/ajouter", name="app_user_new", methods={"GET", "POST"})
+     */
+    public function new(Request $request, UserRepository $userRepository): Response
+    {
+        $user = new User();
+        $form = $this->createForm(UserType::class, $user);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $user->setRoles(['ROLE_ADMIN']);
+            $userRepository->add($user, true);
+
+            return $this->redirectToRoute('app_user_index', [], Response::HTTP_SEE_OTHER);
+        }
+
+        return $this->renderForm('user/new.html.twig', [
+            'user' => $user,
+            'form' => $form,
         ]);
     }
 
@@ -29,7 +52,7 @@ class UserController extends AbstractController
      */
     public function delete(Request $request, User $user, UserRepository $userRepository): Response
     {
-        if ($this->isCsrfTokenValid('delete'.$user->getId(), $request->request->get('_token'))) {
+        if ($this->isCsrfTokenValid('delete' . $user->getId(), $request->request->get('_token'))) {
             $userRepository->remove($user, true);
         }
 
--- src/Form/UserType.php
+++ src/Form/UserType.php
@@ -4,22 +4,86 @@ namespace App\Form;
 
 use App\Entity\User;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\CallbackTransformer;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\DateType;
+use Symfony\Component\Form\Extension\Core\Type\EnumType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
+use Symfony\Component\Form\Extension\Core\Type\PasswordType;
 
 class UserType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('firstname')
-            ->add('lastname')
-            ->add('pseudonym')
-            ->add('email')
-            ->add('roles')
-            ->add('date_of_birth')
-            ->add('created_at');
+            ->add('firstname', TextType::class, [
+                "label" => "Prénom",
+                "attr" => [
+                    "placeholder" => "Prénom"
+                ]
+            ])
+            ->add('lastname', TextType::class, [
+                "label" => "Nom",
+                "attr" => [
+                    "placeholder" => "Nom"
+                ]
+            ])
+            ->add('pseudonym', TextType::class, [
+                "label" => "Pseudo",
+                "attr" => [
+                    "placeholder" => "pseudo"
+                ]
+            ])
+            ->add('email', EmailType::class, [
+                "label" => "Email",
+                "attr" => [
+                    "placeholder" => "pseudo"
+                ]
+
+            ])
+            ->add('password', PasswordType::class, [
+                "label" => "Mot de passe",
+                "attr" => [
+                    "placeholder" => "Mot de passe"
+                ]
+            ])
+            ->add('roles', ChoiceType::class, [
+                "label" => "Role",
+                "attr" => [
+                    "placeholder" => "Role"
+                ],
+                'choices' => [
+                    'Admin' => 'ROLE_ADMIN'
+                ],
+                "disabled" => true
+            ])
+            ->add('date_of_birth', DateType::class, [
+                "label" => "Date de naissance",
+                "attr" => [
+                    "placeholder" => "Date de naissance"
+                ],
+                "widget" => "single_text",
+                "input" => "datetime_immutable",
+            ]);
+
+        //roles field data transformer : https://stackoverflow.com/questions/51744484/symfony-form-choicetype-error-array-to-string-covnersion
+        $builder->get('roles')
+            ->addModelTransformer(new CallbackTransformer(
+                function ($rolesArray) {
+                    // transform the array to a string
+                    return count($rolesArray) ? $rolesArray[0] : null;
+                },
+                function ($rolesString) {
+                    // transform the string back to an array
+                    return [$rolesString];
+                }
+            ));
     }
+
+
 
     public function configureOptions(OptionsResolver $resolver): void
     {
